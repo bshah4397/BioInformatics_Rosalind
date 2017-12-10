2879d10a85a45b3953abcdbf7c91235a08f33177
Bhavya <bshah4397@gmail.com>
2017-12-10 13:18:10 +0530

    Faster algorithm for Rabbit Recurrences program.

--- Rabbits and Recurrence Relations.py
+++ Rabbits and Recurrence Relations.py
@@ -1,3 +1,5 @@
+import time
+
 def Fibonaaci_Rabits(n,k):
     if n==1: return 1
     if n==2: return k
@@ -6,6 +8,18 @@ def Fibonaaci_Rabits(n,k):
     if n<=4: return oneGen+secGen
     return (oneGen + secGen*k)
 
+def Rabbits(n,k):
+    rabbits = [1,1]
+    for i in range(2,n):
+        newRabbits = rabbits[i-2] * k
+        rabbits.append(newRabbits + rabbits[i-1])
+    return rabbits[n-1]
+start = time.time()
+print(Fibonaaci_Rabits(7,3))
+end = time.time()
+print(end - start)
 
-
-print(Fibonaaci_Rabits(34,3))
+start = time.time()
+print(Rabbits(7,3))
+end = time.time()
+print(end - start)
